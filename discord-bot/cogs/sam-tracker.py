eaaa4e5240fa628a1f944d52b8b49b750a4dfa4f
jhayashi1 <jhayashi@iastate.edu>
2023-08-15 16:13:45 -0500

    add sam tracking functionality

--- discord-bot/cogs/sam-tracker.py
+++ discord-bot/cogs/sam-tracker.py
@@ -2,9 +2,8 @@ from discord.ext import commands
 from datetime import datetime, time
 import os
 import discord
-import dotenv
 
-USERNAME = '__jared__'
+USERNAME = 'hotwire12'
 
 class SamTracker(commands.Cog):
     def __init__(self, bot):
@@ -19,11 +18,12 @@ class SamTracker(commands.Cog):
     async def on_presence_update(self, before, after):
         now = datetime.now()
         #Check username -> time greater than 9am and less than 5pm -> day is not weekend -> current status is online -> previous status is not online
-        if before.name != USERNAME or now.time() < time(9) or now.time() > time(17) or now.weekday() >= 5 or after.status != discord.Status.online or before.status == discord.Status.online:
+        if before != USERNAME or now.time() < time(9) or now.time() > time(17) or now.weekday() >= 5 or after.status != discord.Status.online or before.status == discord.Status.online:
             return
-        num_times = int(os.environ['SAM']) + 1
-        os.environ['SAM'] = str(num_times)
-        dotenv.set_key(dotenv.find_dotenv(), 'SAM', os.environ['SAM'])
+
+        num_times = int(os.environ['SAM'])
+        os.environ['SAM'] = num_times + 1
+        
 
 async def setup(bot):
     await bot.add_cog(SamTracker(bot))
